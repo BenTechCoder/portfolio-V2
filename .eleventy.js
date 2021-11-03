11477b628a64270df0e025baa4be37b9178ad0b4
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2021-11-02 20:46:45 -0400

    added incomplete footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/assets");
-  
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {

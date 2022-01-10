5a99888ea7409818268aaabb84d6a8d25a9be6ea
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2022-01-09 19:30:37 -0500

    added 11ty img plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,29 @@
 const socialImages = require("@11tyrocks/eleventy-plugin-social-images");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600, 1000, 1400],
+    formats: ["avif", "webp", "jpeg"]
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
   eleventyConfig.addPlugin(socialImages);
 
   eleventyConfig.addWatchTarget("./src/sass/");
